9ee1df5ee1ca233cefbb907ad338706a77840ad3
Sarah <sarahkwando@gmail.com>
2022-11-03 18:01:32 -0500

    Complete prototypal stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  queue.first = 0;
+  queue.i = 0;
+  queue.storage = {};
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue(value) {
+    this.storage[this.i] = value;
+    this.i += 1;
+    return this.i;
+  },
+  dequeue() {
+    var oldValue = this.storage[this.first];
+    if (this.first < this.i) {
+      this.first += 1;
+    }
+    return oldValue;
+  },
+  size() {
+    return this.i - this.first;
+  }
+};
 
 
