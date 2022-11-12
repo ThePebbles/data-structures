c91f6fadc8c0864b09942c6c952f1747c66bf732
Sarah <sarahkwando@gmail.com>
2022-11-12 11:56:53 -0600

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this['_storage'][item] === undefined) {
+    this['_storage'][item] = 1
+  } else {
+    this['_storage'][item]++;
+  }
 };
 
 setPrototype.contains = function(item) {
+  var result = false;
+  _.each(this['_storage'], function(val, key) {
+    if (key === item) {
+      result = true;
+    }
+  });
+  return result;
 };
 
 setPrototype.remove = function(item) {
+  delete this['_storage'][item];
 };
 
 /*
